583644d48bb4e927367e2e8ef6be78d4cef23ab9
Victor Matias <fvictor1705@gmail.com>
2023-01-18 18:39:13 -0300

    feat: solves requirement 2

---
+++ src/Races/Orc.ts
@@ -0,0 +1,20 @@
+import Race from './Race';
+
+export default class Orc extends Race {
+  private _maxLifePoints: number;
+  private static _instances = 0;
+
+  constructor(name: string, dixterity: number) {
+    super(name, dixterity);
+    this._maxLifePoints = 74;
+    Orc._instances += 1;
+  }
+
+  public get maxLifePoints(): number {
+    return this._maxLifePoints;
+  }
+
+  static createdRacesInstances() {
+    return this._instances;
+  } 
+}
